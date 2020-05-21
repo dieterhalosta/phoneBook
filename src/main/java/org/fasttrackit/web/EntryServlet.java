dd9a827dd1cd412421550e9eff8dd43d8890e6ee
Dieter Halosta <dieter.halosta@yahoo.com>
2020-05-21 16:03:16 +0300

    Started to implement endpoints

--- src/main/java/org/fasttrackit/web/EntryServlet.java
+++ src/main/java/org/fasttrackit/web/EntryServlet.java
@@ -1,6 +1,8 @@
 package org.fasttrackit.web;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.fasttrackit.service.EntryService;
+import org.fasttrackit.transfer.CreateEntryRequest;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,6 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
 
 @WebServlet("/entries")
 public class EntryServlet extends HttpServlet {
@@ -16,6 +19,12 @@ public class EntryServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        CreateEntryRequest request = new ObjectMapper().readValue(req.getReader(), CreateEntryRequest.class);
 
+        try {
+            entryService.createEntry(request);
+        } catch (SQLException | ClassNotFoundException e) {
+           resp.sendError(500, "There was an error reaching the server. " + e.getMessage());
+        }
     }
 }
