b7372dc97049a6658c10a5978724f37e965738a5
Dieter Halosta <dieter.halosta@yahoo.com>
2020-05-26 12:22:46 +0300

    Implemented webservlets

--- src/main/java/org/fasttrackit/web/EntryServlet.java
+++ src/main/java/org/fasttrackit/web/EntryServlet.java
@@ -1,8 +1,12 @@
 package org.fasttrackit.web;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.fasttrackit.config.ObjectMapperConfiguration;
+import org.fasttrackit.domanin.Agenda;
 import org.fasttrackit.service.EntryService;
 import org.fasttrackit.transfer.CreateEntryRequest;
+import org.fasttrackit.transfer.UpdateEntryRequest;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,6 +15,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.List;
 
 @WebServlet("/entries")
 public class EntryServlet extends HttpServlet {
@@ -19,12 +24,52 @@ public class EntryServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        CreateEntryRequest request = new ObjectMapper().readValue(req.getReader(), CreateEntryRequest.class);
+
+
+
+        CreateEntryRequest request = ObjectMapperConfiguration.OBJECT_MAPPER.readValue(req.getReader(), CreateEntryRequest.class);
 
         try {
             entryService.createEntry(request);
         } catch (SQLException | ClassNotFoundException e) {
            resp.sendError(500, "There was an error reaching the server. " + e.getMessage());
+        }
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        final String id = req.getParameter("id");
+
+        UpdateEntryRequest request = ObjectMapperConfiguration.OBJECT_MAPPER.readValue(req.getReader(), UpdateEntryRequest.class);
+
+        try {
+            entryService.updateEntry(Long.parseLong(id), request);
+        } catch (SQLException | ClassNotFoundException e) {
+            resp.sendError(500, "There was an error reaching the server. " + e.getMessage());
+        }
+
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        final String id = req.getParameter("id");
+
+        try {
+            entryService.deleteEntry(Long.parseLong(id));
+        } catch (SQLException | ClassNotFoundException e) {
+            resp.sendError(500, "There was an error reaching the server. " + e.getMessage());
+        }
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            List<Agenda> entries = entryService.getEntry();
+            ObjectMapperConfiguration.OBJECT_MAPPER.writeValue(resp.getWriter(), entries);
+        } catch (SQLException | ClassNotFoundException e) {
+            resp.sendError(500, "There was an error reaching the server. " + e.getMessage());
         }
     }
 }
